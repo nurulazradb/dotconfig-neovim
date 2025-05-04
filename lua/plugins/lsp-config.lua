f6588264de1cc5a3f03f841a325f029353942d88
Azrad M <nurulazrad@gmail.com>
2025-05-04 22:40:49 +0800

    feat: add none-ls to neovim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,8 +20,8 @@ return {
       lspconfig.lua_ls.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
